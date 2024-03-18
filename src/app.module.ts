bc08cf2a7f108ee89ca5a9cae19e2171624dad7e
itsrusty <rusty3031@gmail.com>
2024-03-18 07:41:56 -0700

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { EmployeesModule } from './employees/employees.module';
 import { AdminModule } from './admin/admin.module';
-import { AdminModule } from './admin/admin.module';
+import { AdminController } from './admin/admin.controller';
+import { EmployeesController } from './employees/employees.controller';
+import { AuthController } from './auth/auth.controller';
+import { AdminService } from './admin/admin.service';
+import { EmployeesService } from './employees/employees.service';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [AuthModule, UsersModule, EmployeesModule, AdminModule],
-  controllers: [],
-  providers: [],
+  imports: [AdminModule, EmployeesModule, AuthModule],
+  controllers: [AdminController, EmployeesController, AuthController],
+  providers: [AdminService, EmployeesService, AuthService],
 })
 export class AppModule {}
