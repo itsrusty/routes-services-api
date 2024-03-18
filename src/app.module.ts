5187dea02a003b82087629df59b4fe2b00c48990
itsrusty <rusty3031@gmail.com>
2024-03-18 07:16:00 -0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EmployeesModule } from './employees/employees.module';
+import { AdminModule } from './admin/admin.module';
+import { AdminModule } from './admin/admin.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, EmployeesModule, AdminModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
